1f906ac03466511f275438cd0d6efbfab91fd0ad
Andrew Aposhian <andrewsaposhian@gmail.com>
2020-05-05 20:10:50 -0600

    divide satisfies division algorithm

--- src/lib/division.cpp
+++ src/lib/division.cpp
@@ -2,6 +2,11 @@
 
 std::pair<int, int> divide(int dividend, int divisor) {
     auto r = dividend % divisor;
+
+    if (dividend < 0) {
+        r = divisor < 0 ? r - divisor : r + divisor;
+    }
+
     auto q = (dividend - r) / divisor;
 
     return std::make_pair(q, r);
--- src/spec/division_spec.cpp
+++ src/spec/division_spec.cpp
@@ -59,4 +59,32 @@ SCENARIO("division", "[division]") {
             }
         }
     }
+
+    GIVEN("a zero dividend") {
+        int dividend = 0;
+
+        WHEN("the divisor is positive") {
+            int divisor = 3;
+            auto res = divide(dividend, divisor);
+            auto q = res.first;
+            auto r = res.second;
+
+            THEN("the results satisfy the division algorithm") {
+                REQUIRE((0 <= r && r < std::abs(divisor)));
+                REQUIRE(dividend == divisor * q + r);
+            }
+        }
+
+        WHEN("the divisor is negative") {
+            int divisor = -3;
+            auto res = divide(dividend, divisor);
+            auto q = res.first;
+            auto r = res.second;
+
+            THEN("the results satisfy the division algorithm") {
+                REQUIRE((0 <= r && r < std::abs(divisor)));
+                REQUIRE(dividend == divisor * q + r);
+            }
+        }
+    }
 }
